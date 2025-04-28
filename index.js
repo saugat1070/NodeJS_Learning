f003bcffffe9f6f44d94838759517f52b5647abc
saugat1070 <saugatgiri1070@gmail.com>
2025-04-29 00:22:02 +0545

    connect db in main file

--- index.js
+++ index.js
@@ -1,8 +1,29 @@
 const express = require('express');
+const connect_to_db = require('./DataBase/db_connect');
 const app = express();
 
+connect_to_db()
+
+//Middleware
+// app.use(()=>{
+//     console.log("I am middleware")
+// })
+// app.get('/random', (req,res)=>{
+//     res.send("this is random page")
+// })
+
 app.get('/',(req,res)=>{
-    res.send('thik cha');
+    // res.json({
+    //     message :"Thik cha sabai kura",
+    // });
+    res.status(200).json({
+        message:"Thik cha sabai kura"
+    })
+})
+app.get('/about',(req,res)=>{
+    res.json({
+        'message':'this is about Section'
+    })
 })
 
 app.listen(3000,()=>{
