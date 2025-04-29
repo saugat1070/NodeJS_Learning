2f1f27a18ba78b7379c98dfb0ea5f2d0a794e1a7
saugat1070 <saugatgiri1070@gmail.com>
2025-04-29 22:56:51 +0545

    creating multer middleware to handle image or file upload

---
+++ middleware/mutlerConfig.js
@@ -0,0 +1,12 @@
+const { ModifiedPathsSnapshot } = require('mongoose')
+const multer = require('multer')
+const storage = multer.diskStorage({
+    destination : function(req,file,cb){ //cb-->call back
+        cb(null,'./storage') //cb(error,success)
+    },
+    filename : function(req,file,cb){
+        cb(null,'saugat-'+file.originalname)
+    }
+})
+
+module.exports = {multer,storage}
